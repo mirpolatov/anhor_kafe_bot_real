585a363f22df3cf9be53551a790c9fe284c80d78
miraziz <mirazizmirpolatov9@gmail.com>
2024-02-14 15:38:26 +0500

    yangilandi

--- keyboard_uz.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-keyboard_button = [
-    [KeyboardButton(text='🇺🇿 Uzbek'),
-     KeyboardButton(text='🇷🇺 Russia'),
-     ],
-]
-
-language = ReplyKeyboardMarkup(keyboard=keyboard_button, resize_keyboard=True, row_width=1)
